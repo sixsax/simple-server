9617d64b59c7cc8dc4d9473b552146a72487382d
Six <sirio.salvi@gmail.com>
2019-12-08 07:53:03 +0100

    refactor

--- source/controller/controller.go
+++ source/controller/controller.go
@@ -2,19 +2,19 @@ package controller
 
 import(
 	"net/http"
-    "net/http/httputil"
+	"net/http/httputil"
 	"encoding/json"
 	i "simple-server/source/includes"
 	chain "simple-server/source/validator"
-    log "github.com/sirupsen/logrus"
-    "io/ioutil"
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
 )
 
 type controller struct {
 }
 
 func New() controller{
-    log.SetFormatter(&log.TextFormatter{
+	log.SetFormatter(&log.TextFormatter{
 		DisableColors: false,
 		FullTimestamp: true,
 	})
@@ -24,51 +24,51 @@ func New() controller{
 
 func (c controller) Execute(r *http.Request) (i.ResponseJSON, error) {
 
-    requestDump, err := httputil.DumpRequest(r, true)
-    if err != nil {
-      return nil, err
-    }
+	requestDump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		return nil, err
+	}
 
-    log.Info(string(requestDump))
+	log.Info(string(requestDump))
 
 	action := r.URL.Path
 
 	switch action{
 		case "/validate":
 
-            b, err := ioutil.ReadAll(r.Body)
-            if err != nil {
-                return nil, err
-            }
-            var reqJSON i.RequestJSON
-            json.Unmarshal(b, &reqJSON)
-            if err != nil {
-                return nil, err
-            }
+			b, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				return nil, err
+			}
+			var reqJSON i.RequestJSON
+			json.Unmarshal(b, &reqJSON)
+			if err != nil {
+				return nil, err
+			}
 
 			//The chain that will test the k8s resource
-            ch, err := chain.New(reqJSON)
-            if err != nil {
-                return nil, err
-            }
+			ch, err := chain.New(reqJSON)
+			if err != nil {
+				return nil, err
+			}
 
 			//Execute the chain
 			res := ch.Execute()
 
-			return res, nil
+		return res, nil
 		default:
 			jsonRes := []byte(`{
-		        "response":{
-                        	"message": "Action not recognized!"
-                	}
-		        }`)
+			"response":{
+				"message": "Action not recognized!"
+			}
+			}`)
 
-            var res i.ResponseJSON
-            err := json.Unmarshal(jsonRes, &res)
-            if err != nil {
-                return nil, err
-            }
+			var res i.ResponseJSON
+			err := json.Unmarshal(jsonRes, &res)
+			if err != nil {
+				return nil, err
+			}
 
-            return res, nil
-        }
+		return res, nil
+	}
 }
